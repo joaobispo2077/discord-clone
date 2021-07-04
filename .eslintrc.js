883f67568b3f89822e144600197a0db87886381b
João Bispo <joaobispo2077@gmail.com>
2021-07-04 13:20:33 -0300

    feat: add editorconfig and prettier to enforce code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended", // TypeScript rules
-    "plugin:react/recommended", // React rules
-    "plugin:react-hooks/recommended", // React hooks rules
-    "plugin:jsx-a11y/recommended", // Accessibility rules
-    "plugin:prettier/recommended", // Prettier plugin
-    "plugin:jest/recommended", // Jest plugin
-  ],
-  plugins: ["react", "@typescript-eslint", "jest"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended', // TypeScript rules
+		'plugin:react/recommended', // React rules
+		'plugin:react-hooks/recommended', // React hooks rules
+		'plugin:jsx-a11y/recommended', // Accessibility rules
+		'plugin:prettier/recommended', // Prettier plugin
+		'plugin:jest/recommended', // Jest plugin
+	],
+	plugins: ['react', '@typescript-eslint', 'jest'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
 
-    // We will use TypeScript's types for component props instead
-    "react/prop-types": "off",
+		// We will use TypeScript's types for component props instead
+		'react/prop-types': 'off',
 
-    // Why would you want unused vars?
-    "@typescript-eslint/no-unused-vars": ["error"],
+		// Why would you want unused vars?
+		'@typescript-eslint/no-unused-vars': ['error'],
 
-    // I suggest this setting for requiring return types on functions only where useful
-    "@typescript-eslint/explicit-function-return-type": [
-      "warn",
-      {
-        allowExpressions: true,
-        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-      },
-    ],
-  },
+		// I suggest this setting for requiring return types on functions only where useful
+		'@typescript-eslint/explicit-function-return-type': [
+			'warn',
+			{
+				allowExpressions: true,
+				allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+			},
+		],
+	},
 };
