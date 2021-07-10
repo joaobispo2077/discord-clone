ed60dde73f6c6d93846dc177c4c5c68498242a38
João Bispo <joaobispo2077@gmail.com>
2021-07-10 17:33:41 -0300

    feat: add Layout (grid) component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layout';
 
 function App(): JSX.Element {
 	return (
 		<>
 			<GlobalStyles />
+			<Layout />
 			<h1>Echo Mundo</h1>
 		</>
 	);
