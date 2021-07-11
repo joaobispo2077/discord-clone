003ee713b262836ba784b7573bf0b439f30490a1
João Bispo <joaobispo2077@gmail.com>
2021-07-10 21:20:59 -0300

    feat: add component ServerList

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ServerList from '../ServerList';
 
 import { Grid } from './styles';
 
 const Layout: React.FC = () => {
-	return <Grid />;
+	return (
+		<Grid>
+			<ServerList />
+		</Grid>
+	);
 };
 
 export default Layout;
