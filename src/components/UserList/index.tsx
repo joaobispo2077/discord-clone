9a37d27be719fce0c5047ef91fe7feb695a9764d
João Bispo <joaobispo2077@gmail.com>
2021-07-11 14:18:37 -0300

    feat: add user list component

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { Container, Role, User, Avatar } from './styles';
+
+export interface UserProps {
+	nickname: string;
+	isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+	return (
+		<User>
+			<Avatar className={isBot ? 'bot' : ''} />
+			<strong>{nickname}</strong>
+
+			{isBot && <span>Bot</span>}
+		</User>
+	);
+};
+
+const UserList: React.FC = () => {
+	return (
+		<Container>
+			<Role>Disponível - 1</Role>
+			<UserRow nickname="João Bispo" />
+
+			<Role>Offline - 17</Role>
+			<UserRow nickname="DJ do Lofi" isBot />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+			<UserRow nickname="Fulano" />
+		</Container>
+	);
+};
+
+export default UserList;
